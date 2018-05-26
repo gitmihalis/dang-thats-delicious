538b11a8716d06820f1dcc9c0e4214ae8a38e1d2
Mihalis <mihalisfthenos@icloud.com>
2018-05-26 12:31:41 -0700

    add store singles

--- models/Store.js
+++ models/Store.js
@@ -2,28 +2,61 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
-/* pro tip*** do all data normalization as close to the model as possible */
 const storeSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
+    required: 'Please enter a store name!'
   },
   slug: String,
   description: {
     type: String,
-    trim: true,
+    trim: true
   },
   tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
+  },
+  photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
-    return; // exit this function
+    return; // stop this function from running
   }
   this.slug = slug(this.name);
+  // find other stores that have a slug of wes, wes-1, wes-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
-  // TODO make more resillience so slugs are unique
+  // TODO make more resiliant so slugs are unique
 });
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
+}
 
 module.exports = mongoose.model('Store', storeSchema);
